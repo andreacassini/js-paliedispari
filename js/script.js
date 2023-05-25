c2f838af7c4f75f2d3316913626569ce648ca5ca
andreacassini <andrea1316@hotmail.it>
2023-05-25 16:43:27 +0200

    Add condition

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
-let parola = prompt('');
-console.log(parola);
-
-/* let parolaDivisa = parola.split('');
-console.log(parolaDivisa);
-
-parolaDivisa = parolaDivisa.reverse();
-console.log(parolaDivisa);
-
-parolaDivisa = parolaDivisa.join();
-console.log(parolaDivisa); */
+let parola = prompt('').toLowerCase();
 
 function reverseWord(str){
     let strReverse = str.split('').reverse('').join('');
     return strReverse;
 }
 
+
 let parolaInversa = reverseWord(parola);
-console.log(parolaInversa)
+
+if(parola === parolaInversa){
+    console.log('La parola è palindroma')
+}
+else{
+    console.log("La parola non è palindroma")
+}
