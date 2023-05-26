e19de7104f4803b4d4230a2152f7c60a98ed3dcc
andreacassini <andrea1316@hotmail.it>
2023-05-26 10:32:41 +0200

    secondo problema

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let parola = prompt('').toLowerCase();
+let parola = prompt('Inserire parola per verificare se è palindroma').toLowerCase();
 
 function reverseWord(str){
     let strReverse = str.split('').reverse('').join('');
@@ -13,4 +13,55 @@ if(parola === parolaInversa){
 }
 else{
     console.log("La parola non è palindroma")
+}
+
+
+//secondo problema
+
+//DEFINISCO LA FUNZIONE CHE GENERA IL NUMERO RANDOM
+function randomNumber(){
+    return Math.floor(Math.random() * 6);
+}
+
+//DEFINISCO LA FUNZIONE CHE STABILISCE SE IL NUMERO (SOMMA) E' PARI O DISPARI
+function checkOddEven(num){
+    if(num% 2 === 0){
+        return "pari";
+    }
+
+    return "dispari";
+}
+//PERMETTO ALL'UTENTE DI SCEGLIERE PARI O DISPARI
+let choice = prompt('Inserire "pari" o "dispari');
+
+//PERMETTO ALL'UTENTE DI INSERIRE UN NUMERO COMPRESO TRA 1 E 5
+let number = parseInt(prompt('Inserire un numero compreso tra 1 e 5'));
+
+//CONTROLLO SULLA CONFORMITA' DEL VALORE INSERITO
+while(choice !== 'pari' && choice != 'dispari'){
+    choice = prompt('Inserisci nuovamente la parola chiave "pari" o "dispari"');
+}
+
+//CONTROLLO DELLA CONFORMITA' DEL NUMERO INSERITO
+while(isNaN(number) || number < 1 || number > 5){
+    number = prompt('Inserisci nuovamente il numero');
+}
+
+//DEFINISCO UNA VARIABILE CHE CONTIENE IL NUMERO GENERATO PER IL PC
+let pc_number = randomNumber();
+console.log(pc_number)
+
+//ESEGUO LA SOMMA DEI DUE NUMERI
+let somma = pc_number + number;
+console.log(somma)
+
+//CHIAMO LA FUNZIONE E SALVO IL SUO RISULTATO IN UNA VARIABILE
+let check_odd_even = checkOddEven(somma);
+
+//CONTROLLLIAMO CHI HA VINTO
+if(check_odd_even === choice){ //check_odd_even === true && choice === pari
+    console.log('Vincitore: utente')
+}
+else{
+    console.log('Vincitore: computer')
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
+Problema n.1:
 Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
@@ -12,3 +13,13 @@ Soluzione:
 7 - creo condizione per confrontare le due parole
     7.1 - se le parole sono uguali allora la parola è palindroma
     7.2 - altrimenti no
+
+
+
+Problema n.2:
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+Soluzione:
+
+1 - 
